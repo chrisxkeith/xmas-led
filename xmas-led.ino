34e2d81ce7da52331bcfc66f1a9add11cd1bc764
Chris Keith <chris.keith@gmail.com>
2024-12-04 14:25:01 -0800

    Speed test runs. Move code to class

--- xmas-led.ino
+++ xmas-led.ino
@@ -1,78 +1,41 @@
 #include <FastLED.h>
 
-// How many leds in your strip?
-#define NUM_LEDS 60
+#define DATA_PIN 8
+#define CLOCK_PIN 10
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN (11 - 3)
-#define CLOCK_PIN (13 - 3)
+class App {
+  private:
+    const int         DELAY = 0;
+    static const int  NUM_LEDS = 300;
+    CRGB              leds[NUM_LEDS];
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+    void speedTest() {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::White;
+        FastLED.show();
+        delay(DELAY);
+        leds[i] = CRGB::Black;
+        FastLED.show();
+        delay(DELAY);
+      }
+    }
+  public:
+    void setup() {
+      Serial.begin(115200);
+      delay(1000);
+      Serial.println("setup()");
+      FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+    }
+    void loop() {
+      speedTest();
+    }
+};
+App app;
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("setup()");
-
-  // Uncomment/edit one of the following lines for your leds arrangement.
-  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-
-  // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-
-  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  app.setup();
 }
 
 void loop() {
-  Serial.println("top of loop()");    
-
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Green;
-  FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Blue;
-  FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
+  app.loop();
 }
